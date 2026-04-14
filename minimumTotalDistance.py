79cacbce0c4c0ffad56db974e005c39d2b9f6771
Ch.Srija <chennurisrija05@gmail.com>
2026-04-14 11:13:31 +0530

    Implement minimum total distance calculation

---
+++ minimumTotalDistance.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minimumTotalDistance(self, robot, factory):
+        robot.sort()
+        factory.sort()
+        
+        n, m = len(robot), len(factory)
+        
+        
+        dp = [[float('inf')] * (m + 1) for _ in range(n + 1)]
+        
+        
+        for j in range(m + 1):
+            dp[0][j] = 0
+        
+        for j in range(1, m + 1):
+            pos, limit = factory[j - 1]
+            
+            for i in range(1, n + 1):
+               
+                dp[i][j] = dp[i][j - 1]
+                
+                
+                cost = 0
+                for k in range(1, min(limit, i) + 1):
+                    cost += abs(robot[i - k] - pos)
+                    dp[i][j] = min(dp[i][j], dp[i - k][j - 1] + cost)
+        
+        return dp[n][m]
